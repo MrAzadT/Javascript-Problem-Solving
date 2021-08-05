8b03d73ec94ebe74d30b54b012871c3b81039d4c
azadtamal <mrazadt@gmail.com>
2021-08-05 21:00:05 +0600

    problem

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 // feetToMi
 
-function feetToMil(feet) {
-  var mail = feet / 5280;
-  return mail;
+// function feetToMil(feet) {
+//   var mail = feet / 5280;
+//   return mail;
+// }
+// var getMile = feetToMil(5280);
+// console.log(getMile);
+
+// Celsius to Fahrenheit
+function CelsiusToFahrenheit(Celsius) {
+  var Fahrenheit = (Celsius * 9) / 5 + 32;
+  return Fahrenheit;
 }
-var getMile = feetToMil(5280);
-console.log(getMile);
+var getCelsius = CelsiusToFahrenheit(100);
+
+// function convertToF(celsius) {
+//   return (celsius * 9) / 5 + 32;
+// }
+
+// convertToF(30);
