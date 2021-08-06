581e61e66026a51ba760963e6ebfca71f8d0dd44
azadtamal <mrazadt@gmail.com>
2021-08-06 23:31:27 +0600

    function

--- sum.js
+++ sum.js
@@ -12,17 +12,23 @@
 // var total2 = MyTotal([2, 4, 5, 8, 8]);
 // console.log(total2);
 
-// ====Finding largest integer in an array in JavaScript
+// // ====Finding largest integer in an array in JavaScript
+// function MyTotal(number) {
+//   var largest = 0;
+//   for (let i = 0; i < number.length; i++) {
+//     if (number[i] > largest) {
+//       largest = number[i];
+//     }
+//   }
+//   return largest;
+// }
 
-function MyTotal(number) {
-  var largest = 0;
-  for (let i = 0; i < number.length; i++) {
-    if (number[i] > largest) {
-      largest = number[i];
-    }
-  }
-  return largest;
+// var total2 = MyTotal([2, 4, 5, 8, 18]);
+// console.log(total2);
+
+// ==== Fibonacci
+var fibo = [0, 1];
+for (let i = 2; i < 10; i++) {
+  fibo[i] = fibo[i - 1] + fibo[i - 2];
 }
-
-var total2 = MyTotal([2, 4, 5, 8, 18]);
-console.log(total2);
+console.log(fibo);
