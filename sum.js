4da2861af75d47abe469d28ee5e86ad0553a94ac
azadtamal <mrazadt@gmail.com>
2021-08-06 16:59:31 +0600

    sum

--- sum.js
+++ sum.js
@@ -1,13 +1,28 @@
 // var number = [23, 44, 55, 66, 77, 78];
 
+// function MyTotal(number) {
+//   var sum = 0;
+//   for (let i = 0; i < number.length; i++) {
+//     const element = number[i];
+//     sum = sum + element;
+//   }
+//   return sum;
+// }
+
+// var total2 = MyTotal([2, 4, 5, 8, 8]);
+// console.log(total2);
+
+// ====Finding largest integer in an array in JavaScript
+
 function MyTotal(number) {
-  var sum = 0;
+  var largest = 0;
   for (let i = 0; i < number.length; i++) {
-    const element = number[i];
-    sum = sum + element;
+    if (number[i] > largest) {
+      largest = number[i];
+    }
   }
-  return sum;
+  return largest;
 }
 
-var total2 = MyTotal([2, 4, 5, 8, 8]);
+var total2 = MyTotal([2, 4, 5, 8, 18]);
 console.log(total2);
