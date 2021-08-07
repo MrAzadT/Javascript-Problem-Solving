087727920a87a158ba5cc0f7539ab56485931b17
azadtamal <mrazadt@gmail.com>
2021-08-07 14:08:09 +0600

    function min

--- max.js
+++ max.js
@@ -1,13 +1,29 @@
+// function maxNum(num) {
+//   var max = num[0];
+//   for (let i = 0; i < num.length; i++) {
+//     const element = num[i];
+//     if (element > max) {
+//       max = element;
+//     }
+//   }
+
+//   return max;
+// }
+// var nam = [12, 34, 556, 7, 8867];
+// console.log(maxNum(nam));
+
+// ====min
+
 function maxNum(num) {
-  var max = num[0];
+  var min = num[0];
   for (let i = 0; i < num.length; i++) {
     const element = num[i];
-    if (element > max) {
-      max = element;
+    if (element < min) {
+      min = element;
     }
   }
 
-  return max;
+  return min;
 }
 var nam = [12, 34, 556, 7, 8867];
 console.log(maxNum(nam));
