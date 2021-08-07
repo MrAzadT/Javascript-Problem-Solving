8794b26ccbfefb0afe7d543f43c36adf247b62a3
azadtamal <mrazadt@gmail.com>
2021-08-07 14:06:41 +0600

    function max

---
+++ max.js
@@ -0,0 +1,13 @@
+function maxNum(num) {
+  var max = num[0];
+  for (let i = 0; i < num.length; i++) {
+    const element = num[i];
+    if (element > max) {
+      max = element;
+    }
+  }
+
+  return max;
+}
+var nam = [12, 34, 556, 7, 8867];
+console.log(maxNum(nam));
