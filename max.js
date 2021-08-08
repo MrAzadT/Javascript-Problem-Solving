2062a0c8d975589f45794562834cab46b9c6a088
azadtamal <mrazadt@gmail.com>
2021-08-08 15:00:23 +0600

    function

--- max.js
+++ max.js
@@ -14,19 +14,31 @@
 
 // ====min
 
-function minNum(num) {
-  var min = num[0];
-  for (let i = 0; i < num.length; i++) {
-    const element = num[i];
-    if (element < min) {
-      min = element;
-    }
-  }
+// function minNum(num) {
+//   var min = num[0];
+//   for (let i = 0; i < num.length; i++) {
+//     const element = num[i];
+//     if (element < min) {
+//       min = element;
+//     }
+//   }
 
-  return min;
-}
-var nam = [12, 34, 556, 7, 8867, 67, 87888, 6];
-console.log(minNum(nam));
+//   return min;
+// }
+// var nam = [12, 34, 556, 7, 8867, 67, 87888, 6];
+// console.log(minNum(nam));
 
-for (const iterator of object) {
+// =============
+var i = 5;
+if ((i = 5)) {
+  console.log("hiii");
+} else {
+  console.log("333333");
 }
+
+// for (const iterator of object) {
+// }
+
+// while (condition) {
+
+// }
