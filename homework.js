a8b78b752dcc408cf602888b1c23d123f849e24b
azadtamal <mrazadt@gmail.com>
2021-08-08 21:06:53 +0600

    work

--- homework.js
+++ homework.js
@@ -1,9 +1,18 @@
-// ============== 1 ==========
-var number = 12;
-var myName = "azad";
-var honours = true;
-console.log(number, myName, honours);
+// // ============== 1 ==========
+// var number = 12;
+// var myName = "azad";
+// var honours = true;
+// console.log(number, myName, honours);
 
-// ======== 2 =========
-let fbName = "azad tamal";
-const myAge = 26;
+// // ======== 2 =========
+// let fbName = "azad tamal";
+// const myAge = 26;
+
+//======== 3 =======
+let a = 10;
+let b = 5;
+console.log(a + b);
+console.log(a - b);
+console.log(a / b);
+console.log(a * b);
+console.log(a % b);
