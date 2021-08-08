f113f93980578fadf6c02decbc21d675ec0a87c9
azadtamal <mrazadt@gmail.com>
2021-08-08 16:00:18 +0600

    work

--- homework.js
+++ homework.js
@@ -1,4 +1,9 @@
+// ============== 1 ==========
 var number = 12;
 var myName = "azad";
 var honours = true;
 console.log(number, myName, honours);
+
+// ======== 2 =========
+let fbName = "azad tamal";
+const myAge = 26;
